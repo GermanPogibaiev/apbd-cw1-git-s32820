09da41e2b9e5794fa206231b97c6f8ba1a91d9f8
German Pogibaiev <S32820@pjwstk.edu.pl>
2026-03-17 22:49:28 +0100

    Min Calc

--- StatisticsHelper.cs
+++ StatisticsHelper.cs
@@ -1,48 +1,16 @@
-using System;
-
-public static class StatisticsHelper
+public static int CalculateMin(int[] values)
 {
-    public static double CalculateAverage(int[] values)
-    {
-        ValidateInput(values);
-
-        int sum = 0;
-
-        for (int i = 0; i < values.Length; i++)
-        {
-            sum += values[i];
-        }
-
-        return (double)sum / values.Length;
-    }
-
-    public static int CalculateMax(int[] values)
-    {
-        ValidateInput(values);
-
-        int max = values[0];
-
-        for (int i = 1; i < values.Length; i++)
-        {
-            if (values[i] > max)
-            {
-                max = values[i];
-            }
-        }
-
-        return max;
-    }
-       
-
-    public static void ValidateInput(int[] values)
-    {
-        if (values == null || values.Length == 0)
-        {
-            throw new ArgumentException("Array cannot be null or empty.");
-        }
-    }
-    public static int CalculateMin(int[] values)  {
     ValidateInput(values);
-    return 0;
+
+    int min = values[0];
+
+    for (int i = 1; i < values.Length; i++)
+    {
+        if (values[i] < min)
+        {
+            min = values[i];
+        }
     }
+
+    return min;
 }
