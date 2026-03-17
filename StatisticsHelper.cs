22f3183dca70b5deef5568a61620a78cd4727cc6
German Pogibaiev <S32820@pjwstk.edu.pl>
2026-03-17 22:53:48 +0100

    Restoring prev

--- StatisticsHelper.cs
+++ StatisticsHelper.cs
@@ -1,16 +1,60 @@
-public static int CalculateMin(int[] values)
+using System;
+
+public static class StatisticsHelper
 {
-    ValidateInput(values);
-
-    int min = values[0];
-
-    for (int i = 1; i < values.Length; i++)
+    public static double CalculateAverage(int[] values)
     {
-        if (values[i] < min)
+        ValidateInput(values);
+
+        int sum = 0;
+
+        for (int i = 0; i < values.Length; i++)
         {
-            min = values[i];
+            sum += values[i];
         }
+
+        return (double)sum / values.Length;
     }
 
-    return min;
+    public static int CalculateMax(int[] values)
+    {
+        ValidateInput(values);
+
+        int max = values[0];
+
+        for (int i = 1; i < values.Length; i++)
+        {
+            if (values[i] > max)
+            {
+                max = values[i];
+            }
+        }
+
+        return max;
+    }
+
+    public static int CalculateMin(int[] values)
+    {
+        ValidateInput(values);
+
+        int min = values[0];
+
+        for (int i = 1; i < values.Length; i++)
+        {
+            if (values[i] < min)
+            {
+                min = values[i];
+            }
+        }
+
+        return min;
+    }
+
+    public static void ValidateInput(int[] values)
+    {
+        if (values == null || values.Length == 0)
+        {
+            throw new ArgumentException("Array cannot be null or empty.");
+        }
+    }
 }
