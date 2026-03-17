334cf569c70218ca8c0d334bde58ea990f34d62e
German Pogibaiev <S32820@pjwstk.edu.pl>
2026-03-17 22:31:54 +0100

    Implement avg calc

--- StatisticsHelper.cs
+++ StatisticsHelper.cs
@@ -5,7 +5,15 @@ public static class StatisticsHelper
     public static double CalculateAverage(int[] values)
     {
         ValidateInput(values);
-        return 0;
+
+        int sum = 0;
+
+        for (int i = 0; i < values.Length; i++)
+        {
+            sum += values[i];
+        }
+
+        return (double)sum / values.Length;
     }
 
     public static void ValidateInput(int[] values)
